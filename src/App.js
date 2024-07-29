ff0a9e2fc619769ac8a57b29aa0def18be02347b
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-07-29 00:01:38 -0700

    refactored App as functional component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import JokeList from "./JokeList";
 
 /** App component. Renders list of jokes. */
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <JokeList />
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div className="App">
+      <JokeList />
+    </div>
+  )
 }
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <JokeList />
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
